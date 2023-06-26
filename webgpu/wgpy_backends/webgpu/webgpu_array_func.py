86c6c70c516f62afa7a69a16a69e3c0557c5d486
Masatoshi Hidaka <hidaka@edgeintelligence.jp>
2023-06-26 16:20:39 +0900

    webgpu: matmul

--- webgpu/wgpy_backends/webgpu/webgpu_array_func.py
+++ webgpu/wgpy_backends/webgpu/webgpu_array_func.py
@@ -1,3 +1,4 @@
+import math
 from typing import List, Optional, Tuple, Union
 import numpy as np
 from wgpy_backends.webgpu.platform import get_platform
@@ -78,88 +79,66 @@ class WebGPUArrayFunc:
 
     def matmul(self, lhs: ndarray, rhs: ndarray, out: Optional[ndarray]=None) -> ndarray:
         # 2D only
-        # TODO: irregular texture
+        # TODO: accelerate in special case (such as k is multiple of 4)
         m, k = lhs.shape
         k2, n = rhs.shape
         assert k == k2
-        for in_array in [lhs, rhs]:
-            assert in_array.buffer.texture_shape.dim == '2D'
-            assert in_array.buffer.texture_shape.internal_format in (WebGL2RenderingContext.R32F, WebGL2RenderingContext.R16F)
-            assert in_array.buffer.texture_shape.format == WebGL2RenderingContext.RED
-            assert in_array.buffer.texture_shape.type in (WebGL2RenderingContext.FLOAT, WebGL2RenderingContext.HALF_FLOAT)
-        if out is not None:
-            assert out.flags.c_contiguous_full
-            assert out.buffer.texture_shape.dim == '2D'
-            assert out.buffer.texture_shape.internal_format in (WebGL2RenderingContext.R32F, WebGL2RenderingContext.R16F)
-            assert out.buffer.texture_shape.format == WebGL2RenderingContext.RED
-            assert out.buffer.texture_shape.type in (WebGL2RenderingContext.FLOAT, WebGL2RenderingContext.HALF_FLOAT)
-            for in_array in [lhs, rhs]:
-                assert out.buffer.buffer_id != in_array.buffer.buffer_id
-            assert out.shape == (m, n)
-        kernel_name = f'matmul_{m}_{n}_{k}'
+        kernel_name = f'matmul'
         if kernel_name not in added_kernels:
-            get_platform().addKernel(kernel_name, {'source': f"""#version 300 es
-precision highp float;
-precision highp int;
-precision highp sampler2D;
-precision highp sampler2DArray;
-precision highp isampler2D;
-precision highp isampler2DArray;
-precision highp usampler2D;
-precision highp usampler2DArray;
+            get_platform().addKernel(kernel_name, {'source': """@group(0) @binding(0)
+var<storage,read> array_a: array<f32>;
 
-#define M {m}
-#define N {n}
-#define K {k}
+@group(0) @binding(1)
+var<storage,read> array_b: array<f32>;
 
-uniform int _ka_tex_output_texture_w;
-uniform int LHS_STRIDE_0;
-uniform int LHS_STRIDE_1;
-uniform int RHS_STRIDE_0;
-uniform int RHS_STRIDE_1;
-uniform sampler2D tex_lhs;
-uniform sampler2D tex_rhs;
+@group(0) @binding(2)
+var<storage,read_write> array_c: array<f32>;
 
-out float fragColor;
+struct CMeta {
+  M: f32,
+  N: f32,
+  K: f32,
+  alpha: f32,
+}
 
-float get_tex_lhs(int d0, int d1) {{
-int flat_index = d0 * LHS_STRIDE_0 + d1 * LHS_STRIDE_1;
-int texture_w = textureSize(tex_lhs, 0).x;
-int y = flat_index / texture_w;
-int x = flat_index - y * texture_w;
-return texelFetch(tex_lhs, ivec2(x, y), 0).r;
-}}
+@group(0) @binding(3)
+var<storage,read> cmeta: CMeta;
 
-float get_tex_rhs(int d0, int d1) {{
-int flat_index = d0 * RHS_STRIDE_0 + d1 * RHS_STRIDE_1;
-int texture_w = textureSize(tex_rhs, 0).x;
-int y = flat_index / texture_w;
-int x = flat_index - y * texture_w;
-return texelFetch(tex_rhs, ivec2(x, y), 0).r;
-}}
-
-
-void main() {{
-    int flat_idx = int(gl_FragCoord.x) + int(gl_FragCoord.y) * _ka_tex_output_texture_w;
-    int oi = flat_idx / N;
-    int oj = flat_idx - oi * N;
-    if (oi >= M) {{ return; }}
-    float s = 0.0;
-    for (int k = 0; k < K; k++) {{
-        s += get_tex_lhs(oi, k) * get_tex_rhs(k, oj);
-    }}
-    fragColor = s;
-}}
-    """})
+@compute @workgroup_size(8,8,1)
+fn main(
+  @builtin(global_invocation_id) global_id: vec3<u32>
+) {
+  var M: u32 = u32(cmeta.M);
+  var N: u32 = u32(cmeta.N);
+  var K: u32 = u32(cmeta.K);
+  var x: u32 = global_id.x;
+  var y: u32 = global_id.y;
+  if (x >= N || y >= M) {
+    return;
+  }
+  var sum: f32 = 0.0;
+  for(var k: u32 = 0u; k < K; k = k + 1u) {
+    sum = array_a[y * K + k] * array_b[k * N + x] + sum;
+  }
+  array_c[x + y * N] = sum * cmeta.alpha;
+}
+    """,
+                'bindingTypes': ['read-only-storage', 'read-only-storage', 'storage', 'read-only-storage'],})
             added_kernels.add(kernel_name)
+        # TODO: consider strides
+        assert lhs.flags.c_contiguous_full
+        assert rhs.flags.c_contiguous_full
+        # TODO: allow mix of int32 (size), float32 (alpha)
+        meta = WebGPUBuffer(4, np.dtype(np.float32))
+        meta.set_data(np.array([m, n, k, 1.0], dtype=np.float32))
         if out is None:
             out = ndarray((m, n), lhs.dtype)
-        get_platform().runKernel({'name': kernel_name, 'inputs': [{'name': 'tex_lhs', 'id': lhs.buffer.buffer_id}, {
-                     'name': 'tex_rhs', 'id': rhs.buffer.buffer_id}], 'output': out.buffer.buffer_id, 'uniforms': [
-                        {'name': '_ka_tex_output_texture_w', 'value': out.buffer.texture_shape.width, 'type': 'int'},
-                        {'name': 'LHS_STRIDE_0', 'value': lhs.strides[0] // lhs.itemsize, 'type': 'int'},
-                        {'name': 'LHS_STRIDE_1', 'value': lhs.strides[1] // lhs.itemsize, 'type': 'int'},
-                        {'name': 'RHS_STRIDE_0', 'value': rhs.strides[0] // rhs.itemsize, 'type': 'int'},
-                        {'name': 'RHS_STRIDE_1', 'value': rhs.strides[1] // rhs.itemsize, 'type': 'int'}
-                        ]})
+        else:
+            assert out.flags.c_contiguous_full
+
+        get_platform().runKernel({
+            'name': kernel_name,
+            'tensors': [lhs.buffer.buffer_id, rhs.buffer.buffer_id, out.buffer.buffer_id, meta.buffer_id],
+            'workGroups': {'x': int(math.ceil(n/8)), 'y': int(math.ceil(m/8)), 'z': 1},
+        })
         return out
